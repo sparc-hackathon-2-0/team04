cc18c1a715a3ee0d3095cd0d8d48acc7b3ae3f75
Jason Rikard <jsnrkd@gmail.com>
2012-08-25 20:26:18 -0400

    Loads detail item view

--- ParseStarterProject/ItemDetailViewController.h
+++ ParseStarterProject/ItemDetailViewController.h
@@ -10,5 +10,7 @@
 #import <Parse/Parse.h>
 
 @interface ItemDetailViewController : UIViewController
-
+@property (retain, nonatomic) IBOutlet UILabel *titlex;
+@property (retain, nonatomic) IBOutlet UILabel *description;
+- (id)initWithSupply:(PFObject*)supply;
 @end
--- ParseStarterProject/ItemDetailViewController.m
+++ ParseStarterProject/ItemDetailViewController.m
@@ -13,6 +13,8 @@
 @end
 
 @implementation ItemDetailViewController
+@synthesize titlex;
+@synthesize description;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -25,7 +27,20 @@
 
 -(id) initWithIndex:(NSIndexPath*)index
 {
+    titlex.text = @"test";
+    description.text = @"test";
     
+    return self;
+}
+
+- (id)initWithSupply:(PFObject*)supply
+{
+    
+    if (self) {
+        NSString *title = [supply objectForKey:@"title"];
+        
+        self.title = title;
+    }
     
     return self;
 }
@@ -33,11 +48,18 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    description.lineBreakMode = UILineBreakModeWordWrap;
+    description.numberOfLines = 0;
+    [description sizeToFit];
+    
     // Do any additional setup after loading the view from its nib.
 }
 
 - (void)viewDidUnload
 {
+    [self setTitle:nil];
+    [self setDescription:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -48,5 +70,10 @@
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
+- (void)dealloc {
+    [titlex release];
+    [description release];
+    [super dealloc];
+}
 @end
 
